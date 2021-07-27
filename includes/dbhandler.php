b113312c9f837d1691ea262ea2abd86ff33cda96
soup-tech <maximilliancampbell@gmail.com>
2021-07-26 21:46:06 -0400

    Created header and added styling

---
+++ includes/dbhandler.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "localhost";
+$dbuname = "root";
+$dbpass = "G0dH@d3s";
+$dbname = "capstone";
+
+$conn = mysqli_connect($servername,$dbuname,$dbpass,$dbname);
+
+if (!$conn) {
+	echo "Failed to connect to MYSQL: ". mysqli_connect_error();
+}
+
+echo "Successfully connected to mysql server";
---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Login</title>
+		<link rel="stylesheet" type="text/css" href="/css/header.css" />
+	</head>
+
+	<body>
+		<div class="header">
+			<a href="#default" class="logo">Our_Logo</a>
+			<div class="header-right">
+				<a class="active" href="#home">Home</a>
+				<a href="#contact">Contact</a>
+				<a href="#about">About</a>
+			</div>
+		</div>
+	</body>
+
+</html>

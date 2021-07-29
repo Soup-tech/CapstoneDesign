936c3c6bf4136dd0bb8cf44867dc66c5efb0197c
soup-tech <campbellssoup21@gmail.com>
2021-07-29 17:38:59 -0400

    Began on raspberry pi

---
+++ raspberry-pi/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+########################################
+## Created by Maximillian A. Campbell ##
+########################################
+
+# ======= Modules =======
+from gpiozero import Button, LED
+from signal import pause
+# ======= Variables =======
+button = Button(2)
+led = LED(17)
+
+
+# ======= Main =======
+

d164b2f8f9e38841fda8e25cf87f32bd7e367887
dzidarov98 <dz0005@mix.wvu.edu>
2021-10-31 14:20:49 -0400

    made formatting changes to about-us

--- about-us.php
+++ about-us.php
@@ -5,9 +5,11 @@
 
 <DOCTYPE html>
     <html lang="en">
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 
     <head>
         <link rel="stylesheet" type="text/css" href="css/about-us.css">
+    
     </head>
 
     <body>
@@ -21,59 +23,49 @@
         <h2 style="text-align:center">Our Team</h2>
 
         <!-- Card for every person -->
-        <div class="row">
-
-            <div class="column">
-                <div class="card">
-                    <img src="" alt="Name" style="width:100%">
-                    <div class="container">
-                        <h2>Person</h2>
-                        <p class="title">Team Member</p>
-                        <p>Description/Biography/Studying?</p>
-                        <p>contain info</p>
+        <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <div class="card ">
+                            <img src="" alt="Name" style="width:100%"></img>
+                                <h2>Person</h2>
+                                <p class="title">Team Member</p>
+                                <p>Description/Biography/Studying?</p>
+                                <p>contain info</p>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card">
+                            <img src="" alt="Name" style="width:100%"></img>
+                                <h2>Person</h2>
+                                <p class="title">Team Member</p>
+                                <p>Description/Biography/Studying?</p>
+                                <p>contain info</p>
+                        </div>
+                    </div>
+                <div class="col-3">
+                    <div class="card">
+                            <img src="" alt="Name" style="width:100%"></img>
+                                <h2>Person</h2>
+                                <p class="title">Team Member</p>
+                                <p>Description/Biography/Studying?</p>
+                                <p>contain info</p>
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="card">
+                        <img src="" alt="Name" style="width:100%"></img>
+                            <h2>Person</h2>
+                            <p class="title">Team Member</p>
+                            <p>Description/Biography/Studying?</p>
+                            <p>contain info</p>
                     </div>
                 </div>
             </div>
-
-            <div class="column">
-                <div class="card">
-                    <img src="" alt="Name" style="width:100%">
-                    <div class="container">
-                        <h2>Person</h2>
-                        <p class="title">Team Member</p>
-                        <p>Description/Biography/Studying?</p>
-                        <p>contain info</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="column">
-                <div class="card">
-                    <img src="" alt="Name" style="width:100%">
-                    <div class="container">
-                        <h2>Person</h2>
-                        <p class="title">Team Member</p>
-                        <p>Description/Biography/Studying?</p>
-                        <p>contain info</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="column">
-                <div class="card">
-                    <img src="" alt="Name" style="width:100%">
-                    <div class="container">
-                        <h2>Person</h2>
-                        <p class="title">Team Member</p>
-                        <p>Description/Biography/Studying?</p>
-                        <p>contain info</p>
-                    </div>
-                </div>
-            </div>
-
         </div>
+
 
 
     </body>
 
-    </html>
+</html>
--- history/history-menu.php
+++ history/history-menu.php
@@ -15,7 +15,7 @@
         <meta charset="utf-8">
         <title>History Menu</title>
         <style>
-            body { font: 14px sans-serif; }
+            body { font: 14px sans-serif; background-image: url('../includes/backr.jpg');}
 		    .wrapper { width: 360px; padding: 20px; margin: 100px auto;}
             .history-menu { height: 700px; width: 1550px; border: 1px solid #4e4e4e; font: 16px Arial, Serif; overflow: auto; margin: 20px; float: left; }
             /* .menu-wrapper { width: 500px; border: 1px solid black; overflow: hidden; margin: 20px; } */
@@ -23,6 +23,7 @@
             .alerts { width: 150px; float: left; border: 1px solid red; margin-left: 700px; text-align: center; }
             .medication-amount { width: 150px; border: 1px solid green; margin-left: 20px; text-align: center; overflow: hidden; float: left;  }
             .export { width: 150px; border: 1px solid blue; margin-left: 20px; text-align: center; overflow: hidden; float: left; }
+            
         </style>
     </head>
 
